98f760cc5ca18b528a9fd99b436b98dc6de5177b
Funny_Silkie <ban.grumble99rar@gmail.com>
2021-11-03 02:14:38 +0900

    implement auto compensantation

--- Asd2Edittor/Views/Behaviors/MainTextBoxBehavior.cs
+++ Asd2Edittor/Views/Behaviors/MainTextBoxBehavior.cs
@@ -1,10 +1,13 @@
 ﻿using Asd2Edittor.Messangers;
+using System;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Asd2Edittor.Views.Behaviors
 {
     public class MainTextBoxBehavior : RxMessageBehavior<TextBox>
     {
+        private string currentEntered;
         public MainTextBoxBehavior()
         {
 
@@ -18,6 +21,38 @@ namespace Asd2Edittor.Views.Behaviors
                     AssociatedObject.GetBindingExpression(TextBox.TextProperty).UpdateSource();
                     break;
             }
+        }
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.PreviewTextInput += OnPreviewTextInput;
+        }
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.PreviewTextInput -= OnPreviewTextInput;
+        }
+        private void OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            currentEntered = e.Text;
+            switch (currentEntered)
+            {
+                case "<": InsertText(">"); break;
+                case "\"": InsertText("\""); break;
+            }
+        }
+        private void InsertText(string inserted)
+        {
+            var position = AssociatedObject.SelectionStart;
+            var length = AssociatedObject.SelectionLength;
+            var text = AssociatedObject.Text;
+            AssociatedObject.BeginChange();
+            AssociatedObject.Clear();
+            AssociatedObject.AppendText(text[0..position]);
+            AssociatedObject.AppendText(inserted);
+            AssociatedObject.AppendText(text[position..]);
+            AssociatedObject.EndChange();
+            AssociatedObject.SelectionStart = position;
         }
     }
 }
