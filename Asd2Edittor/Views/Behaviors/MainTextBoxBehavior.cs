cfa65513c07d64c04d3fe51218ef13853e5073b8
Funny_Silkie <ban.grumble99rar@gmail.com>
2021-11-03 23:41:14 +0900

    implement auto indent

--- Asd2Edittor/Views/Behaviors/MainTextBoxBehavior.cs
+++ Asd2Edittor/Views/Behaviors/MainTextBoxBehavior.cs
@@ -6,7 +6,6 @@ namespace Asd2Edittor.Views.Behaviors
 {
     public class MainTextBoxBehavior : RxMessageBehavior<TextBox>
     {
-        private string currentEntered;
         public MainTextBoxBehavior()
         {
 
@@ -25,18 +24,54 @@ namespace Asd2Edittor.Views.Behaviors
         {
             base.OnAttached();
             AssociatedObject.PreviewTextInput += OnPreviewTextInput;
-            AssociatedObject.KeyDown += KeyDown;
+            AssociatedObject.PreviewKeyDown += OnPreviewKeyDown;
         }
         protected override void OnDetaching()
         {
             base.OnDetaching();
             AssociatedObject.PreviewTextInput -= OnPreviewTextInput;
-            AssociatedObject.KeyDown -= KeyDown;
+            AssociatedObject.PreviewKeyDown -= OnPreviewKeyDown;
+        }
+        private void OnPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyboardDevice.Modifiers)
+            {
+                case ModifierKeys.Control:
+                    switch (e.Key)
+                    {
+                        case Key.D:
+                            CtrlD();
+                            break;
+                    }
+                    break;
+                case ModifierKeys.None:
+                    switch (e.Key)
+                    {
+                        case Key.Enter:
+                            var (x, y) = AssociatedObject.GetCaretPosition();
+                            var line = AssociatedObject.GetLineText(y);
+                            var c = 0;
+                            for (int i = 0; i < line.Length; i++)
+                            {
+                                if (line[i] != ' ') break;
+                                c++;
+                            }
+                            AssociatedObject.InsertText($"\r\n{new string(' ', c)}");
+                            AssociatedObject.CaretIndex += 2 + c;
+                            e.Handled = true;
+                            break;
+                        case Key.Tab:
+                            AssociatedObject.InsertText("    ");
+                            AssociatedObject.CaretIndex += 4;
+                            e.Handled = true;
+                            break;
+                    }
+                    break;
+            }
         }
         private void OnPreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-            currentEntered = e.Text;
-            switch (currentEntered)
+            switch (e.Text)
             {
                 case "/":
                     AssociatedObject.InsertText("/>");
@@ -44,6 +79,7 @@ namespace Asd2Edittor.Views.Behaviors
                     e.Handled = true;
                     break;
                 case "\"": AssociatedObject.InsertText("\""); break;
+                case "{": AssociatedObject.InsertText("}"); break;
                 case ">":
                     var start = AssociatedObject.CaretIndex;
                     for (int i = start - 1; i >= 0; i--)
@@ -56,8 +92,10 @@ namespace Asd2Edittor.Views.Behaviors
                     for (int i = start + 1; i < AssociatedObject.CaretIndex; i++)
                     {
                         if (AssociatedObject.Text[i] is '>' or ' ') break;
+                        if (AssociatedObject.Text[i] == '/') return;
                         count++;
                     }
+                    if (count == 0) return;
                     var name = AssociatedObject.Text.Substring(start + 1, count);
                     AssociatedObject.InsertText($"></{name}>");
                     AssociatedObject.CaretIndex++;
@@ -68,19 +106,8 @@ namespace Asd2Edittor.Views.Behaviors
                     AssociatedObject.CaretIndex += 2;
                     e.Handled = true;
                     break;
-            }
-        }
-        private void KeyDown(object sender, KeyEventArgs e)
-        {
-            switch (e.KeyboardDevice.Modifiers)
-            {
-                case ModifierKeys.Control:
-                    switch (e.Key)
-                    {
-                        case Key.D:
-                            CtrlD();
-                            break;
-                    }
+                case "\r\n":
+
                     break;
             }
         }
