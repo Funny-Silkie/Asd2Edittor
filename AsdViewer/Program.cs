c2b185d17e25b5ebcebee469efc50f82bcd26120
Funny_Silkie <ban.grumble99rar@gmail.com>
2021-11-07 01:56:27 +0900

    fix size

--- AsdViewer/Program.cs
+++ AsdViewer/Program.cs
@@ -7,10 +7,18 @@ namespace AsdViewer
     {
         static void Main(string[] args)
         {
-            if (!Engine.Initialize("", 960, 720)) return;
+            if (!Engine.Initialize("", 726, 500)) return;
             Engine.ClearColor = new Color(200, 200, 200);
+            var rect = new RectangleNode
+            {
+                Color = new Color(255, 0, 0),
+                Position = new Vector2F(100, 100),
+                RectangleSize = new Vector2F(250, 40)
+            };
+            Engine.AddNode(rect);
             while (Engine.DoEvents())
             {
+                rect.Angle++;
                 Engine.Update();
             }
             Engine.Terminate();
