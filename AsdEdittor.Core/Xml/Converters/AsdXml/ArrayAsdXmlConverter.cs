fdd4275c5d15bcec86c0ea3a18bc97b34d2addd2
Funny_Silkie <ban.grumble99rar@gmail.com>
2021-11-20 05:03:44 +0900

    add ArrayConverter

---
+++ AsdEdittor.Core/Xml/Converters/AsdXml/ArrayAsdXmlConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Asd2UI.Xml.Converters
+{
+    /// <summary>
+    /// 一次元配列を変換する<see cref="AsdXmlConverter{T}"/>のクラス
+    /// </summary>
+    public class ArrayAsdXmlConverter : AsdXmlConverter<Array>
+    {
+        private readonly Type elementType;
+        /// <summary>
+        /// <see cref="ArrayAsdXmlConverter"/>の新しいインスタンスを初期化する
+        /// </summary>
+        /// <param name="elementType">要素の型</param>
+        /// <exception cref="ArgumentNullException"><paramref name="elementType"/>がnull</exception>
+        public ArrayAsdXmlConverter(Type elementType)
+        {
+            this.elementType = elementType ?? throw new ArgumentNullException(nameof(elementType), "引数がnullです");
+        }
+        /// <inheritdoc/>
+        public override bool CanParse(Type type) => type.IsArray;
+        /// <inheritdoc/>
+        public override bool Convert(XmlEntry xml, AsdXmlReader reader, out Array result)
+        {
+            var list = new List<object>();
+            var converter = reader.AsdXmlConverterProvider.GetConverter(elementType);
+            foreach (var children in xml.Children)
+            {
+                converter.Convert(children, elementType, reader, out var element);
+                list.Add(element);
+            }
+            result = Array.CreateInstance(elementType, list.Count);
+            for (int i = 0; i < list.Count; i++) result.SetValue(list[i], i);
+            return true;
+        }
+    }
+}
--- AsdEdittor.Core/Xml/Converters/AsdXml/DefaultAsdXmlConverterProvider.cs
+++ AsdEdittor.Core/Xml/Converters/AsdXml/DefaultAsdXmlConverterProvider.cs
@@ -14,6 +14,7 @@ namespace Asd2UI.Xml.Converters
             switch (type)
             {
                 case null: throw new ArgumentNullException(nameof(type), "引数がnullです");
+                case Type t when t.IsArray: return new ArrayAsdXmlConverter(type.GetGenericArguments()[0]);
                 case Type t when IsNodeType(t): return (AsdXmlConverter)Activator.CreateInstance(typeof(NodeAsdXmlConverter<>).MakeGenericType(type));
                 default: return (AsdXmlConverter)Activator.CreateInstance(typeof(DefaultAsdXmlConverter<>).MakeGenericType(type));
             }
